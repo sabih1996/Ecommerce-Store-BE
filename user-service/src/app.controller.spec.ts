fda226ac4a4ddeeeda1813134f442ec9c21e76d0
Sabih Ul Hassan <47629282+sabih1996@users.noreply.github.com>
2024-07-29 19:10:14 +0500

    test

--- user-service/src/app.controller.spec.ts
+++ user-service/src/app.controller.spec.ts
@@ -14,7 +14,7 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('Hello world', () => {
+  describe('Hello', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
